ef701980702a377c4feeee177570355cabe102af
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-06-11 18:01:15 +0700

    untuk preorder

--- searchTree.cpp
+++ searchTree.cpp
@@ -95,4 +95,18 @@ public:
         }
     }
 
-    
+    void preorder(node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout <<"Tree is empty" <<  endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
